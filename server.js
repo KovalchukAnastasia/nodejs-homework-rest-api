378b8fdd93217f9b9d862784b8dc26090d840805
KovalchukAnastasia <103355891+KovalchukAnastasia@users.noreply.github.com>
2023-02-22 17:45:25 +0200

    add_hw_03

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+// const DB_HOST =
+//   "mongodb+srv://kovalchukanastasia:SdRIPUHpdZ3jwJqu@cluster0.thdnwxa.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const app = require("./app");
+const { DB_HOST } = process.env;
+mongoose.set("strictQuery", true);
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
