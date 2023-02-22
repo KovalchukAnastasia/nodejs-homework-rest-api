ee5facc9f5234521d545beae653816e235d658d2
KovalchukAnastasia <103355891+KovalchukAnastasia@users.noreply.github.com>
2023-02-22 18:03:57 +0200

    add_anv_axample

--- server.js
+++ server.js
@@ -2,21 +2,18 @@ const mongoose = require("mongoose");
 // const DB_HOST =
 //   "mongodb+srv://kovalchukanastasia:SdRIPUHpdZ3jwJqu@cluster0.thdnwxa.mongodb.net/db-contacts?retryWrites=true&w=majority";
 const app = require("./app");
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
+
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
-  .then(() => {
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
-    });
-  })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    })
+  )
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
